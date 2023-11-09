3bffd6530b06bbf07a5e42f8ddf3b40e8be42364
roberto.medeiros <roberto_p.v@outlook.com>
2023-11-08 22:00:56 -0300

    primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div style={{maxWidth: "100%"}}>
+      <header style={{display: "flex"}}>
+        <div style={{display: "flex", width: "100%", padding: "27px"}}>
+          <h1>
+            <img style={{display: "block", width: "100px", height: "59px"}} src="./senac_logo.png" />
+          </h1>
+          <div style={{display: "flex", width: "100%", justifyContent: "center", alignContent: "center"}}>
+            <p style={{fontSize: "35px", color: "#004587"}}>Teste</p>
+          </div>
+        </div>
       </header>
+      <div style={{display: "flex", padding: "0 40px", backgroundImage:"url(http://localhost:3000/slide_senac.jpg", backgroundSize: "cover", height: "200px", alignItems: "center"}}>
+        <p style={{color: "white", fontSize: "24px"}}>Senac</p> 
+      </div>
     </div>
   );
 }
