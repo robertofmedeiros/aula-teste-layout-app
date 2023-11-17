966cc4300daba7121021331f3b525cb31604ed31
ro057549 <roberto.medeiros@intelbras.com.br>
2023-11-17 15:03:07 -0300

    Adicionando router

--- src/App.tsx
+++ src/App.tsx
@@ -40,20 +40,6 @@ const rows = [
 
 function App() {
   return (
-    <div style={{maxWidth: "100%"}}>
-      <header style={{display: "flex"}}>
-        <div style={{display: "flex", width: "100%", padding: "27px"}}>
-          <h1>
-            <img style={{display: "block", width: "100px", height: "59px"}} src="./senac_logo.png" />
-          </h1>
-          <div style={{display: "flex", width: "100%", justifyContent: "center", alignContent: "center"}}>
-            <p style={{fontSize: "35px", color: "#004587"}}>Teste</p>
-          </div>
-        </div>
-      </header>
-      <div style={{display: "flex", padding: "0 40px", backgroundImage:"url(http://localhost:3000/slide_senac.jpg", backgroundSize: "cover", height: "200px", alignItems: "center"}}>
-        <p style={{color: "white", fontSize: "24px"}}>Senac</p> 
-      </div>
       <div style={{display: "flex"}}>
         <DataGrid
           rows={rows}
@@ -69,7 +55,6 @@ function App() {
           disableRowSelectionOnClick
         />
       </div>
-    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter, RouterProvider
+} from "react-router-dom";
+import HeaderApp from './components/HeaderApp';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  }
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <HeaderApp>
+      <RouterProvider router={router} />
+    </HeaderApp>
   </React.StrictMode>
 );
 
